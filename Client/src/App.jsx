ac25c78d88884a445564e6d0f66a387ab48607ae
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-07 18:46:11 +0530

    added pages and routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import { Loginpage } from './Pages/Loginpage'
+import Profilepage from './Pages/Profilepage'
+ 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element = {<Home />}/>
+        <Route path='/login' element = {<Loginpage />} />
+        <Route path='/profile' element = {<Profilepage/>} />
+      </Routes>
     </div>
   )
 }
