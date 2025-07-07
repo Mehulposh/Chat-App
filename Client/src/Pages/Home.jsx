a180aa9907662dcd39580a72ef65ee9023845599
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-07 19:29:22 +0530

    added siderbar logo menu and search

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import Rightbar from '../components/Rightbar'
 
 const Home = () => {
+    const [selectedUser, setSelectedUser] = useState(false)
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] '>
+        <div className={`border-2 border-gray-600 rounded-2xl overflow-hidden 
+            h-[100%] grid grid-cols-2 relative backdrop-blur-xl ${selectedUser ? 
+            "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2" }`}
+        >
+            <Sidebar />
+            <ChatContainer />
+            <Rightbar />
+        </div>
     </div>
   )
 }
