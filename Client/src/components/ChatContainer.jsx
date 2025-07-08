131c64a0cac4dabb37cbf71a2fc22270228dd128
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-08 15:46:13 +0530

    added jsx for messages

--- Client/src/components/ChatContainer.jsx
+++ Client/src/components/ChatContainer.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useRef,useEffect } from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+ 
 
-const ChatContainer = () => {
-  return (
-    <div className='text-white'>ChatContainer</div>
+const ChatContainer = ({setSelectedUser,selectedUser}) => {
+    const scrollEnd = useRef();
+
+    useEffect(() => {
+        if(scrollEnd.current){
+            scrollEnd.current.scrollIntoView({behavior: 'smooth'})
+        }
+    },[])
+
+
+return selectedUser? (
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+        <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+            <img src={assets.profile_martin} className='rounded-full w-8'/>
+            <p className='flex-1 text-lg text-white  flex items-center gap-2 ' >
+                Martin
+                <span className='w-2 h-2 rounded-full bg-green-300'></span>
+            </p>
+            <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} className='md:hidden max-w-7'/>
+            <img src={assets.help_icon} className='max-md:hidden max-w-5'/>
+        </div>
+
+        <div className='flex flex-col h-[calc(100% - 120)] overflow-y-scroll p-3 pb-6'>
+            {messagesDummyData.map((msg,idx) => (
+                <div key={idx} className= {`flex items-end justify-end gap-2 ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && "flex-row-reverse"}`}>
+                    {msg.image ? (
+                        <img src={msg.image} className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
+                    ) : (
+                        <p className= {`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8  break-all bg-violet-500/30
+                             text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? "rounded-br-none" : "rounded-bl-none"}`}
+                        >
+                            {msg.text}
+                        </p>
+                    )}
+                    <div className='text-center text-xs'>
+                        <img 
+                            src={msg.senderId === '680f50e4f10f3cd28382ecf9' ?
+                                assets.avatar_icon : assets.profile_martin} 
+                            className='rounded-full w-7' 
+
+                        />
+                        <p className='text-gray-500'>{msg.createdAt}</p>
+                    </div>
+                </div>
+            ))}
+            <div ref={scrollEnd}>
+
+            </div>
+        </div>
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+        <img src={assets.logo_icon} className='max-w-16 ' />
+        <p className='text-lg text-white font-medium'>Chat anytime anywhere</p>
+    </div>
   )
 }
 
