bd32f187ae5c4ec65310a1ecb06a5aab71df2705
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-09 23:23:43 +0530

    added post api for sending messages

--- Server/routes/messageRoutes.js
+++ Server/routes/messageRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.js';
-import { getMessages, getUsers, markMsgAsSeen } from '../controllers/messageController.js';
+import { getMessages, getUsers, markMsgAsSeen, sendMessage } from '../controllers/messageController.js';
 
 const messageRouter = express.Router();
 
 messageRouter.get('/users', protectRoute, getUsers);
 messageRouter.get('/:id', protectRoute, getMessages);
 messageRouter.put('mark/:id', protectRoute, markMsgAsSeen);
+messageRouter.post('/send/:id', protectRoute, sendMessage);
+
 
 export default messageRouter;
