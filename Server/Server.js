82e656408767f9282e706fc940a983c1bc556478
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-09 22:50:25 +0530

    added socket io setup

--- Server/Server.js
+++ Server/Server.js
@@ -5,12 +5,46 @@ import http from 'http';
 import { connectDB } from './LIB/db.js';
 import userRouter from './routes/userRoute.js';
 import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
+import { Socket } from 'dgram';
+import { log } from 'console';
 
 //Create Express app and HTTP server 
 
 const app = express();
 
 const server = http.createServer(app);
+
+//initialize socket.io server
+export const io = new Server (Server , {
+    cors: {origin: "*"},
+
+})
+
+//store online users
+export const userSocketMap = {}; //{userId: socketId }
+
+//socket.io connection handler
+io.on('connection' , (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log('User connected',userId);
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+
+    //emit online user to all connected clients
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnectd' , userId);
+        delete userSocketMap[userId];
+        io.emit('getOnlineUsers' , Object.keys(userSocketMap));
+        
+    })
+    
+})
+
 
 //Middleware setup
 
