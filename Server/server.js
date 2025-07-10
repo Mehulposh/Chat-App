c8e8b4b83e647ee840255c51c897d5dcb564cfac
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-10 19:25:11 +0530

    removed if in server.js

--- Server/server.js
+++ Server/server.js
@@ -33,11 +33,11 @@ app.use('/api/messages',messageRouter);
 //Connect to DB
 await connectDB();
 
-if(process.env.NODE_ENV !== 'production'){
-    const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT , ()=> console.log('server is running on PORT:' + PORT));
-}
+
 
 //export server for vercel
 export default server;
